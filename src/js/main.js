aef3c481d5c1eb82b32a312cb001fba2a05785cc
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-01-24 11:20:04 -0800

    getting api data

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import '@fortawesome/fontawesome-free/js/all';
 import '../assets/sass/styles.scss';
 import { formPrevent, getFormData, searchBtn } from './dom';
+import { weatherApi } from './api';
 
 let data;
 
@@ -9,6 +10,18 @@ formPrevent();
 searchBtn.addEventListener('click', () => {
 
     data = getFormData();
+
+    if (data) {
+        weatherApi(data.city, data.units).then(json  => {
+
+            if (json.cod === 200) {
+
+            } else {
+
+            }
+
+        });
+    }
 
 });
 
