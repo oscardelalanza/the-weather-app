a402d4c16d5b2d52be7ec1b036446943b3610e23
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-01-25 00:02:03 -0800

    redundant await

--- src/js/api.js
+++ src/js/api.js
@@ -3,7 +3,7 @@ const key = '291f2db75d12957358be3e8e29761005';
 const weatherApi = async (city, units = 'metric') => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${key}`;
   const response = await fetch(url);
-  return await response.json();
+  return response.json();
 };
 
 export {
